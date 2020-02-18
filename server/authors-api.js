8e4e041c833752bb10681dcc92eb7ef80b8026c9
duncanwebb9499 <webbd2@xavier.edu>
2020-02-18 16:59:05 -0500

    inital edit from class

--- server/authors-api.js
+++ server/authors-api.js
@@ -5,5 +5,6 @@ const authors = require('./authors');
 
 router.get('/', (req, res) => authors.getAll(req, res));
 router.get('/:id', (req, res) => authors.getById(req, res));
+router.get('/:id/books', (req, res) => authors.getByAuthorId(req, res));
 
 module.exports = router;
