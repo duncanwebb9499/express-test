0110e212d5e85ffff3071f3cc019130d9e673e86
duncanwebb9499 <webbd2@xavier.edu>
2020-02-18 17:06:55 -0500

    Corrected router

--- server/authors.js
+++ server/authors.js
@@ -8,7 +8,14 @@ const authors = {
       return res.sendStatus(404);
     }
   },
-
+  getById: (req, res) => {
+    const books = bookList.filter(a => a.AuthorId == req.params.id);
+    if (books && books.length) {
+      return res.status(200).send(books);
+    } else {
+      return res.sendStatus(404);
+    }
+  },
   getById: (req, res) => {
     const author = authorList.filter(a => a.Id == req.params.id);
     if (author && author.length) {
